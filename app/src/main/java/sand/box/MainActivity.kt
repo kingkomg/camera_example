608cff7dd01d18791a4b4e52f1f3683a3d257c0b
Round Rabbit <roundrabbit@gmail.com>
2019-04-23 18:15:31 +0200

    Initial

---
+++ app/src/main/java/sand/box/MainActivity.kt
@@ -0,0 +1,15 @@
+package sand.box
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        savedInstanceState ?: supportFragmentManager.beginTransaction()
+            .replace(R.id.container, Camera2BasicFragment.newInstance())
+            .commit()
+    }
+}
